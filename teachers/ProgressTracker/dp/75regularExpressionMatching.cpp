9087ee500748569eb54b9b71590e7afc211ae446
Vinay Kumar <vinaykumar@sunlife.com>
2024-01-16 12:33:04 +0530

    kj

--- teachers/ProgressTracker/dp/75regularExpressionMatching.cpp
+++ teachers/ProgressTracker/dp/75regularExpressionMatching.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool fun(int i, int j, string &s, string &p, vector<vector<int>> &dp)
+bool match(int i, int j, string &s, string &p, vector<vector<int>> &dp)
 {
     if (i == s.length() && j == p.length())
         return true;
-    if (j == p.length())
+    if (j >= p.length())
         return false;
-    bool match = (i < s.length() && (s[i] == p[j] || p[j] == '.'));
+    if (i < s.length() && dp[i][j] != -1)
+        return dp[i][j];
+    bool matchChar = i < s.length() && (s[i] == p[j] || p[j] == '.');
     if (j + 1 < p.length() && p[j + 1] == '*')
-        return dp[i][j] = (fun(i, j + 2, s, p, dp) || (match && fun(i + 1, j, s, p, dp)));
-    if (match)
-        return dp[i][j] = fun(i + 1, j + 1, s, p, dp);
+        return dp[i][j] = (match(i, j + 2, s, p, dp) || (matchChar && match(i + 1, j, s, p, dp)));
+    if (matchChar)
+        return dp[i][j] = match(i + 1, j + 1, s, p, dp);
     return dp[i][j] = false;
 }
 bool isMatch(string s, string p)
 {
     vector<vector<int>> dp(s.length() + 1, vector<int>(p.length() + 1, -1));
-    return fun(0, 0, s, p, dp);
+    return match(0, 0, s, p, dp);
 }
---
+++ teachers/ProgressTracker/tree/77inorderTraversalOfBinaryTree.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+vector<int> ans;
+void inorder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    ans.push_back(root->val);
+    inorder(root->right);
+}
+vector<int> inorderTraversal(TreeNode *root)
+{
+    if (root == NULL)
+        return ans;
+    inorder(root);
+    return ans;
+}
+vector<int> inorderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+        return ans;
+    stack<TreeNode *> st;
+    TreeNode *node = root;
+    while (true)
+    {
+        if (node != NULL)
+        {
+            st.push(node);
+            node = node->left;
+        }
+        else
+        {
+            if (st.empty() == true)
+                break;
+            node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            node = node->right;
+        }
+    }
+    return ans;
+}
+
+vector<int> inorders(TreeNode *root)
+{
+    if (!root)
+        return;
+    vector<int> ans;
+    stack<TreeNode *> st;
+    st.push(root);
+    while (!st.empty())
+    {
+        if (root)
+        {
+            st.push(root);
+            root = root->left;
+        }
+        else
+        {
+            if (st.empty())
+                break;
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            root = root->right;
+        }
+    }
+    return ans;
+}
